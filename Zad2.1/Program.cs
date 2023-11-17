6b1cfa6d586b61c77dfbe9b5bdc776abe6f33267
biQte <patrykgonet@gmail.com>
2023-11-17 12:24:16 +0100

    Add project files.

---
+++ Zad2.1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Zad2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj długość w calach:");
+            float inches = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Długość w centymetrach wynosi: " + (inches * 2.54) + " cm");
+        }
+    }
+}
